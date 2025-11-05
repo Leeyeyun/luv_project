1afd031044aa0245218bd0e7015524099598b340
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2025-11-05 15:58:02 +0900

    header 수정

--- script/common.js
+++ script/common.js
@@ -9,19 +9,6 @@ window.addEventListener("scroll", () => {
     }
 });
 
-// main-lovelanguages 마우스 트래킹
-const container = document.querySelector('.lovelanguages');
-const items = document.querySelectorAll('.item_area .item');
-
-let mouseX = 0, mouseY = 0;
-let currentX = 0, currentY = 0;
-
-container.addEventListener('mousemove', (e) => {
-    const rect = container.getBoundingClientRect();
-    mouseX = e.clientX - rect.width / 2;
-    mouseY = e.clientY - rect.height / 2;
-});
-
 //header sub 나오도록
 document.addEventListener('DOMContentLoaded', function() {
     const fontsMenu = document.querySelector('.gnb > li:nth-child(2)');
--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+// main-lovelanguages 마우스 트래킹
+const container = document.querySelector('.lovelanguages');
+const items = document.querySelectorAll('.item_area .item');
+
+let mouseX = 0, mouseY = 0;
+let currentX = 0, currentY = 0;
+
+container.addEventListener('mousemove', (e) => {
+    const rect = container.getBoundingClientRect();
+    mouseX = e.clientX - rect.width / 2;
+    mouseY = e.clientY - rect.height / 2;
+});
+
 function animate() {
     // 부드럽게 따라가기
     currentX += (mouseX - currentX) * 0.08;
